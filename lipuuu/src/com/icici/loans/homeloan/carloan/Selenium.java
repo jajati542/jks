97e0f025e959908e52ec9916c05c2c48131fc07e
jajati542 <60706011+jajati542@users.noreply.github.com>
2020-02-10 20:28:04 +0530

    Update Selenium.java

--- lipuuu/src/com/icici/loans/homeloan/carloan/Selenium.java
+++ lipuuu/src/com/icici/loans/homeloan/carloan/Selenium.java
@@ -1,7 +1,14 @@
 package com.icici.loans.homeloan.carloan;
 
 public class Selenium {
-
+public void add ()
+	int a,b,d
+}
+	public static void main(String[] args) {
+		System.out.println("selenium program");
+	System.out.println("added project");
+	System.out.println("countries canada");
+	}
 	public static void main(String[] args) {
 		System.out.println("selenium program");
 	System.out.println("added project");
