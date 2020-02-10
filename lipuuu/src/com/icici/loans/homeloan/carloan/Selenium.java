dbd4df1d91f1d963103e6f15ff20002799e79381
jajati542 <60706011+jajati542@users.noreply.github.com>
2020-02-10 16:07:39 +0530

    added 1st line

--- lipuuu/src/com/icici/loans/homeloan/carloan/Selenium.java
+++ lipuuu/src/com/icici/loans/homeloan/carloan/Selenium.java
@@ -3,8 +3,7 @@ package com.icici.loans.homeloan.carloan;
 public class Selenium {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("selenium program");
 	}
 
 }
