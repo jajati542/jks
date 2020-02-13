73ba24353d9e55f16168048ccada0c373612d906
jajati542 <60706011+jajati542@users.noreply.github.com>
2020-02-13 12:20:52 +0530

    adding bbbbb project to github server

---
+++ bbbbb/src/com/sbi/loan/carloan/homeloan/Thirdclass.java
@@ -0,0 +1,29 @@
+package com.sbi.loan.carloan.homeloan;
+
+public class Thirdclass
+{
+
+	int a=10,b=20,c;
+	public void add()
+	{
+		c=a+b;
+	System.out.println("Addition of A&B is:"+c);
+	}
+	public void sub()
+	{
+		c=a-b;
+		System.out.println("substraction of A&B is:"+c);
+	}
+public static void main(String[] args) 
+{
+	System.out.println("hiiiiii");
+	Thirdclass obj=new Thirdclass();
+	obj.add();
+	obj.sub();
+}
+	}
+
+
+
+
+
